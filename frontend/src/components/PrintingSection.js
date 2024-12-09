9712dbd81b8fa0322fcde1abec729daa06eaaf78
ijasahamedstr <ijasahamed.str@gmail.com>
2024-12-09 17:18:40 +0200

    commit

--- frontend/src/components/PrintingSection.js
+++ frontend/src/components/PrintingSection.js
@@ -150,8 +150,8 @@ const PrintingSection = () => {
     pauseOnHover: true,
     responsive: [
       { breakpoint: 1200, settings: { slidesToShow: 4 } },
-      { breakpoint: 900, settings: { slidesToShow: 3 } },
-      { breakpoint: 600, settings: { slidesToShow: 2 } },
+      { breakpoint: 900, settings: { slidesToShow: 2 } },
+      { breakpoint: 600, settings: { slidesToShow: 1 } },
     ],
   };
 
