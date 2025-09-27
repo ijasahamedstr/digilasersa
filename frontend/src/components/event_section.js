1671716fd7e4f1565b824cf43af73269f807c122
ijasahamedstr <ijasahamed.str@gmail.com>
2025-09-27 11:13:36 +0300

    commit

--- frontend/src/components/event_section.js
+++ frontend/src/components/event_section.js
@@ -132,7 +132,7 @@ function Eventsection() {
                             sx={{
                               width: "100%",
                               height: "100%",
-                              objectFit: "cover",
+                              objectFit: "contain",
                               borderRadius: "8px",
                             }}
                           />
