a0b4c697094e838136e05f6500ed5e3a0ce05f7f
ijasahamedstr <ijasahamed.str@gmail.com>
2025-09-27 09:50:28 +0300

    commit

--- frontend/src/components/Slider.js
+++ frontend/src/components/Slider.js
@@ -1,5 +1,5 @@
 import { Carousel } from "react-bootstrap";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import {
   FaInstagram,
   FaLinkedin,
@@ -10,21 +10,12 @@ import {
 } from "react-icons/fa";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
-import demoVideo10 from "./video/Vid-20250925-Wa0006new.mp4";
+
+import demoVideo from "./video/slider.mp4";
 
 const carouselItems = [
-  {
-    id: 1,
-    video: demoVideo10,
-  },
-  {
-    id: 2,
-   video: demoVideo10,
-  },
-  {
-    id: 3,
-   video: demoVideo10,
-  },
+  { id: 1, video: demoVideo },
+  { id: 2, video: demoVideo },
 ];
 
 const socialLinks = [
@@ -40,99 +31,118 @@ const socialLinks = [
     icon: <FaLinkedin size={25} />,
     link: "https://www.linkedin.com/company/digilasersa",
   },
-  { icon: <FaYoutube size={25} />, link: "https://youtube.com/@digilaserSa" },
+  {
+    icon: <FaYoutube size={25} />,
+    link: "https://youtube.com/@digilaserSa",
+  },
   {
     icon: <FaSnapchat size={25} />,
     link: "https://www.snapchat.com/add/digilasersa",
   },
-  { icon: <FaTiktok size={25} />, link: "https://www.tiktok.com/@digilasersa" },
-  { icon: <FaWhatsapp size={25} />, link: "http://wa.me/966571978888" },
+  {
+    icon: <FaTiktok size={25} />,
+    link: "https://www.tiktok.com/@digilasersa",
+  },
+  {
+    icon: <FaWhatsapp size={25} />,
+    link: "http://wa.me/966571978888",
+  },
 ];
 
 const FadeCarousel = () => {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        position: "relative",
-        overflow: "hidden",
-        marginTop: "100px",
-      }}
-    >
-      <Carousel
-        fade
-        controls
-        indicators
-        nextIcon={
-          <span
-            className="carousel-control-next-icon"
-            style={{ backgroundColor: "black" }}
-          />
-        }
-        prevIcon={
-          <span
-            className="carousel-control-prev-icon"
-            style={{ backgroundColor: "black" }}
-          />
-        }
-      >
-        {carouselItems.map((item) => (
-          <Carousel.Item key={item.id}>
-            <video
-              className="d-block w-100"
-              src={item.video}
-              autoPlay
-              loop
-              muted
-              playsInline
-              style={{
-                objectFit: "cover",
-                height: "100vh", // fullscreen height
-              }}
+      <Box sx={{ width: "100%", position: "relative", overflow: "hidden",mt: { xs: 5,  mt: "100px" }}}>
+        {/* Carousel */}
+        <Carousel
+          fade
+          nextIcon={
+            <span
+              className="carousel-control-next-icon"
+              style={{ backgroundColor: "black" }}
             />
-          </Carousel.Item>
-        ))}
-      </Carousel>
+          }
+          prevIcon={
+            <span
+              className="carousel-control-prev-icon"
+              style={{ backgroundColor: "black" }}
+            />
+          }
+        >
+          {carouselItems.map((item) => (
+            <Carousel.Item key={item.id}>
+              <video
+                className="d-block w-100"
+                src={item.video}
+                autoPlay
+                loop
+                muted
+                playsInline
+                style={{
+                  objectFit: "cover",
+                  boxShadow: "inset 0 0 10px rgba(0, 0, 0, 0.8)",
+                }}
+              />
+              <Carousel.Caption>
+                <Typography
+                  variant="h4"
+                  sx={{
+                    color: "white",
+                    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
+                  }}
+                >
+                  {item.title}
+                </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{
+                    color: "white",
+                    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
+                  }}
+                >
+                  {item.content}
+                </Typography>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
 
-      {/* Social Media Icons Sidebar */}
-      <Box
-        sx={{
-          position: "fixed",
-          top: "50%",
-          left: 0,
-          transform: "translateY(-50%)",
-          display: { xs: "none", md: "flex" },
-          flexDirection: "column",
-          gap: 2,
-          zIndex: 1200,
-          pl: 2,
-        }}
-      >
-        {socialLinks.map(({ icon, link }, index) => (
-          <a key={index} href={link} target="_blank" rel="noopener noreferrer">
-            <Box
-              sx={{
-                width: 40,
-                height: 40,
-                borderRadius: "50%",
-                backgroundColor: "#06f9f3",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                color: "#17202a",
-                boxShadow: 3,
-                transition: "transform 0.3s ease",
-                "&:hover": {
-                  transform: "scale(1.2)",
-                },
-              }}
-            >
-              {icon}
-            </Box>
-          </a>
-        ))}
+        {/* Social Media Icons */}
+        <Box
+          sx={{
+            position: "fixed",
+            top: "50%",
+            left: 0,
+            transform: "translateY(-50%)",
+            display: { xs: "none", md: "flex" },
+            flexDirection: "column",
+            gap: 2,
+            zIndex: 1200,
+            pl: 2,
+          }}
+        >
+          {socialLinks.map(({ icon, link }, index) => (
+            <a key={index} href={link} target="_blank" rel="noopener noreferrer">
+              <Box
+                sx={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: "50%",
+                  backgroundColor: "#06f9f3",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  color: "#17202a",
+                  boxShadow: 3,
+                  transition: "transform 0.3s ease",
+                  "&:hover": { transform: "scale(1.2)" },
+                }}
+              >
+                {icon}
+              </Box>
+            </a>
+          ))}
+        </Box>
       </Box>
-    </Box>
   );
 };
 
