f431080ee7ca9849288c6b1d944e73b4773b07d8
ijasahamedstr@gmail.com <ijasahamed.str@gmail.com>
2025-10-15 21:48:57 +0300

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,9 +34,9 @@ import ComingSoonPage from "./components/ComingSoonPage";
 import ContactusForm from "./components/Contact";
 import SoundSection from "./components/Sound";
 import WebsiteSection from "./components/websiteSection";
-import Votemain from "./components/Votemain";
-import Vote from "./components/Vote";
-import VoteImage from "./components/Voteimage";
+// import Votemain from "./components/Votemain";
+// import Vote from "./components/Vote";
+// import VoteImage from "./components/Voteimage";
 
 const theme = createTheme({
   typography: {
@@ -123,11 +123,11 @@ function App() {
           <Route path="/إتصل بنا" element={<ContactusForm />} />
           <Route path="/update/:id" element={<UpdateFild />} />
           <Route path="/قسم الصوتيات" element={<SoundSection />} />
-          {/* <Route path="/قسم الفن التشكيلي" element={<ComingSoonPage />} /> */}
-          <Route path="/تصويت" element={<Vote />} />
+          <Route path="/قسم الفن التشكيلي" element={<ComingSoonPage />} />
+          {/* <Route path="/تصويت" element={<Vote />} />
           <Route path="/قسم الفن التشكيلي" element={<Votemain />} />
           <Route path="/تصويت" element={<Vote />} />
-          <Route path="/لوحات الكنفس" element={<VoteImage />} />
+          <Route path="/لوحات الكنفس" element={<VoteImage />} /> */}
         </Routes>
          {footerVisible && <Footer />}
       </Router>
