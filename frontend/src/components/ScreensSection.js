32fc613de3d6174cdc2b22718d89713255bb95f7
ijasahamedstr <ijasahamed.str@gmail.com>
2025-11-16 12:56:24 +0300

    commit

--- frontend/src/components/ScreensSection.js
+++ frontend/src/components/ScreensSection.js
@@ -365,7 +365,7 @@ const ScreensSection = () => {
       src="https://i.ibb.co/4gVbPdHj/5.webp"
       alt="WebP Image"
       style={{
-        objectFit: "contain",
+        objectFit: "cover",
         width: "100%",
         height: "60%",
         display: "block",
